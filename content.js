e73042dd90a4847ad15f447348da9ebfdca725f4
hsm-hx <skytea922@gmail.com>
2020-05-24 07:01:11 +0900

    リプレースをランダムにした

--- content.js
+++ content.js
@@ -1,8 +1,13 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 setInterval(() => {
   tweets = Array.from(document.getElementsByClassName('tweet-text'));
   tweets.forEach((tweet) => {
     if (tweet.innerHTML.includes('地雷')) {
-      tweet.innerHTML = 'あくあくんかわいい';
+      index = getRandomInt(happyTweets.length)
+      tweet.innerHTML = happyTweets[index];
     }
   })
 }, 5000)
